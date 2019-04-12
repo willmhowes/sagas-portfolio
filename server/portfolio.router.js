bd114f6af991d1b08a341fd26b1695b985cfbae7
Will Howes <willmhowes@gmail.com>
2019-04-12 16:17:19 -0500

    make so server is operable

---
+++ server/portfolio.router.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const pool = require('./pool');
+
+const router = express.Router();
+
+router.get('/projects', (req, res) => {
+   const queryText = `SELECT * FROM "projects";`;
+
+   pool.query(queryText)
+      .then((result) => {
+         res.send(result.rows);
+      }).catch((err) => {
+         console.log('Error getting project data', err);
+         res.sendStatus(500);
+      });
+});
+
+router.get('/tags', (req, res) => {
+   const queryText = `SELECT * FROM "tags";`;
+
+   pool.query(queryText)
+      .then((result) => {
+         res.send(result.rows);
+      }).catch((err) => {
+         console.log('Error getting tag data', err);
+         res.sendStatus(500);
+      });
+});
+
+router.get('/project%tags', (req, res) => {
+   const queryText = `SELECT * FROM "project_tags";`;
+
+   pool.query(queryText)
+      .then((result) => {
+         res.send(result.rows);
+      }).catch((err) => {
+         console.log('Error getting project/tag data', err);
+         res.sendStatus(500);
+      });
+});
+
+module.exports = router;
