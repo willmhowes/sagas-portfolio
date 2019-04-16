48efd4d5295b255799c2faed364196845c93db75
Will Howes <willmhowes@gmail.com>
2019-04-15 21:28:49 -0500

    make so App displays reducer values

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount = () => {
+    this.props.dispatch({ type: 'GET_PROJECTS' });
+  }
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
+        {JSON.stringify(this.props.reduxState.projects)}
+        {JSON.stringify(this.props.reduxState.tags)}
         <p>Empty Page</p>
       </div>
     );
   }
 }
 
-export default App;
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState
+});
+
+export default connect(mapReduxStateToProps)(App);
